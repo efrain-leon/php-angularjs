d61f2e684e9afb5e1904daa628a1b204182a4e88
efrain.leon <efrain.leonh@gmail.com>
2015-03-17 18:09:10 -0500

    first commit

---
+++ client/app/scripts/home/controllers/home.js
@@ -0,0 +1,47 @@
+'use strict';
+
+angular.module('Home').controller('home', function($scope, $http) {
+  
+  $scope.names = [ ];
+  
+	$http.get('../server/model.php').success(function(data) {
+    $scope.names = eval(data);
+    console.log(data)
+  })
+  .error(function(data) {
+    console.log('Error: ' + data);
+	});
+  
+  $scope.addNom = function() {
+    $http.post('../server/model.php', { op: 'append', nom: $scope.nom, telefon: $scope.telefon } ).success(function(data) {
+      $scope.names = eval(data);
+      console.log(data)
+    })
+    .error(function(data) {
+      console.log('Error: ' + data);
+    });
+    
+    $scope.nom="";
+    $scope.telefon="";
+  }
+  
+  $scope.delNom = function( nom ) {
+    if ( confirm("Seguro?") ) {
+      $http.post('../server/model.php', { op: 'delete', nom: nom } ).success(function(data) {
+        $scope.names = eval(data);
+        console.log(data)
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+    }
+  }
+ 
+})
+.config(function ($routeProvider) {
+  $routeProvider
+    .when('/home',{
+      templateUrl: 'scripts/home/views/home.html',
+      controller: 'home'
+    });
+});
---
+++ client/app/scripts/home/module.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('Home', [
+  'ngAnimate',
+  'ngAria',
+  'ngCookies',
+  'ngMessages',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'ngRoute'
+]);
